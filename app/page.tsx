726f9fd90c663250505414a54c4dc11391866f50
Caleb <calebnull88@outlook.com>
2025-11-17 17:00:19 -0600

    minor ui changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+// 1. Import the wave.svg file
+import Wave from "@/public/wave.svg";
+import Image from "next/image"; // Needed for optimized SVG usage
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center bg-sky-300">
-      <div className="space-y-6">
+    <main className="relative flex h-screen flex-col items-center justify-center bg-hero-gradient overflow-hidden">
+      <div className="relative z-10 space-y-6 text-center">
         <h1 className="text-6xl font-semibold text-white drop-shadow-md">
           Auth
         </h1>
