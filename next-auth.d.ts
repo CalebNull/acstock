bdddec08a46efa354069ae9447180ee5f3476337
Caleb <calebnull88@outlook.com>
2025-11-19 21:29:27 -0600

    Added users verification date

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -4,12 +4,22 @@ import NextAuth, { type DefaultSession } from "next-auth";
 /**
  * Extends interface to include role
  */
-export type EntendedUser = DefaultSession["user"] & {
+export type ExtendedUser = DefaultSession["user"] & {
   role: UserRole;
+  emailVerified: Date | null;
 };
 
 declare module "next-auth" {
   interface Session {
     user: ExtendedUser;
+  }
+}
+
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: UserRole;
+    emailVerified?: Date | null;
   }
 }
