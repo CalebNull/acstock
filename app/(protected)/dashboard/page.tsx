781c0d317bc9b14824b0188ee0d15af89c6b097c
Caleb <calebnull88@outlook.com>
2025-11-19 21:30:43 -0600

    Created dashboard page and moved settings.

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { auth, signOut } from "@/auth";
+import { db } from "@/lib/db";
 
-export const Dashboard = () => {
+
+
+// Needs to be changed. Just wanted to find user info from db.
+
+export default async function Dashboard() {
+    const session = await auth();
+    console.log(session)
+
     return (
         <>
-            <div>Dashboard</div>
+            <div className="flex items-center justify-center flex-col gap-y-[10px]">
+                {JSON.stringify(session)}
+                <form
+                    action={async () => {
+                        "use server";
+                        await signOut();
+                    }}
+                >
+                    <button type="submit" className="cursor-pointer bg-blue-300 rounded-sm p-1.5">
+                        Sign Out
+                    </button>
+                </form>
+            </div>
         </>
     )
 }
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,23 +1,8 @@
-import { auth, signOut } from "@/auth";
-import React from "react";
 
-const SettingsPage = async () => {
-  const session = await auth();
+const SettingsPage = () => {
   return (
     <>
-      <div className="">
-        {JSON.stringify(session)}
-        <form
-          action={async () => {
-            "use server";
-            await signOut();
-          }}
-        >
-          <button type="submit" className="cursor-pointer bg-red-600">
-            Sign Out
-          </button>
-        </form>
-      </div>
+      Settings Page
     </>
   );
 };
